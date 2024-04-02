8b9d37afd2ef71a5853c73e2019ad8c3832ea35f
WallalaIronRed <jaranguiz07@gmail.com>
2024-04-01 21:07:38 -0300

    agregar js

---
+++ assets/js/js.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function(){
+    $("#destacados").click(function(){
+        $(".card-img-top").toggle();
+        });
+        });
+
+$(document).ready(function(){
+    $("#quienes").click(function(){
+        $(this).toggle();
+        });
+        });
+
+
+
+$(document).ready(function(){
+$('[data-toggle="tooltip"]').tooltip();   
+});
